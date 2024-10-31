17fe4cb7b3d1d9adbbe2ac8bc0d8bb794dfe341a
Nguyen Luong Gia Bao <nguyenluonggiabao301203@gmail.com>
2024-10-31 14:52:38 +0700

    Image manage

--- ChipChipShop.ViewModels/Catalog/ProductImanges/ProductImageViewModel.cs
+++ ChipChipShop.ViewModels/Catalog/ProductImanges/ProductImageViewModel.cs
@@ -4,15 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChipChipShop.ViewModels.Catalog.Products
+namespace ChipChipShop.ViewModels.Catalog.ProductImanges
 {
     public class ProductImageViewModel
     {
         public int Id { get; set; }
 
-        public string FilePath { get; set; }
+        public int ProductId { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string Caption { get; set; }
 
         public bool IsDefault { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public int SortOrder { get; set; }
 
         public long FileSize { get; set; }
     }
