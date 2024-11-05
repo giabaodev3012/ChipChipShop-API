ae12110e415043cd8059db87664bf6f980a3afdb
LyThiHoangYen <lythihoangyen20052003@gmail.com>
2024-11-06 03:43:38 +0700

    ok

---
+++ ChipChipShop/ChipChipShop.ViewModels/Catalog/Products/GetPublicProductPagingRequest.cs
@@ -0,0 +1,14 @@
+﻿using ChipChipShop.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChipChipShop.ViewModels.Catalog.Products
+{
+    public class GetPublicProductPagingRequest : PagingRequestBase
+    {
+        public int? CategoryId { get; set; }
+    }
+}
